1545dbf9bed72c138f62680657e6ece3a3cf01c5
jose-34 <odhiambojoseph491@gmail.com>
2022-04-11 16:07:45 +0300

    shading the bottom triangle

--- image.py
+++ image.py
@@ -1,18 +1,24 @@
 #creating png image
 import sys
-import math
+# import math
 from PIL import Image, ImageDraw
 w, h = 300, 300
 
-shape = [(-100, -100), (w - 1, h - 1)] 
+# shape = [(-100, -100), (w - 1, h - 1)] 
 
 # shape1 = [(-100, -100), (w - 100, h - 100)] 
  
 img = Image.new('RGB', (w, h), color = 'grey')
+img.save('pil_red.png')
 with Image.open("pil_red.png") as img:
+
+    # drawing diagonal lines
     draw = ImageDraw.Draw(img)
     draw.line((0, 0) + img.size, fill="red")
     draw.line((0, img.size[1], img.size[0], 0), fill="green")
+
+    # for coloring the triangle formed at the bottom
+    draw.polygon([(150, 150), (300, 300), (0, 300)], fill=(0, 0, 255),outline=(0, 0, 0))
 
      # write to stdout
     img.save(sys.stdout, "PNG")
@@ -23,6 +29,6 @@ with Image.open("pil_red.png") as img:
 # img1.line(shape, fill ="red", width = 0)
 # img1.line(shape1, fill = "blue", width = 0)
 
-# img.save('pil_red.png')
+img.save('pil_red.png')
 
 img.show()
